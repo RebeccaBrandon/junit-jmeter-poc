258950449d4d9626491a58feeedf0faf5caa02a2
RebeccaBrandon <beccabrandon@hotmail.com>
2022-02-26 21:46:20 +0000

    Temporary (hopefully) fix for finding appconfig.properties

--- jmeter-poc/src/main/java/helpers/AppConfig.java
+++ jmeter-poc/src/main/java/helpers/AppConfig.java
@@ -1,20 +1,32 @@
 package helpers;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 public class AppConfig {
 	public boolean isLoaded = false;
 	Properties properties = new Properties();
-
+	public String pathToResource = System.getenv("JMETER_POC_APP_CONFIG_PATH");
+				
 	public String getSetting(String settingName) {
 		if (!isLoaded) {
-			try {
+			try
+			{
 				loadConfig();
-			} catch (IOException e) {
-				e.printStackTrace();
 			}
+			catch(IOException | NullPointerException ex)
+			{
+				try {
+					loadConfigWithFileReader();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			
 		}
 
 		return properties.getProperty(settingName);
@@ -23,11 +35,31 @@ public class AppConfig {
 	public int getSettingInt(String settingName) {
 			return Integer.parseInt(this.getSetting(settingName));
 	}
-
+    
 	private void loadConfig() throws IOException {
-		InputStream configFileStream = AppConfig.class.getClassLoader().getResourceAsStream("appconfig.properties");
-		this.properties.load(configFileStream);
-		configFileStream.close();
+        InputStream configFileStream = AppConfig.class.getClassLoader().getResourceAsStream("appconfig.properties");
+        this.properties.load(configFileStream);
+        configFileStream.close();
+    }
+	
+	private void loadConfigWithFileReader() throws IOException {
+		if(pathToResource == null)
+		{
+			System.out.println("env. variable JMETER_POC_APP_CONFIG_PATH not set");
+			pathToResource = "C://temp/appconfig.properties";
+		}
+		
+		System.out.println("Checking " + pathToResource + " for app config");
+		
+		FileReader fr = null;
+		try {
+		    fr = new FileReader(Paths.get(pathToResource).toFile());
+		} catch (FileNotFoundException e) {
+		    System.err.println("Couldn't load resources file!");
+		    e.printStackTrace();
+		}
+		this.properties.load(fr);
+		fr.close();
 		this.isLoaded = true;
 	}
 }
--- jmeter-poc/src/test/java/test/Main.java
+++ jmeter-poc/src/test/java/test/Main.java
@@ -1,8 +1,16 @@
 package test;
 
+import helpers.AppConfig;
+
 public class Main
 {
 	public static void main(String[] args){
-		
+		AppConfig appConfig = new AppConfig();
+		System.out.println("Run tests by using JUnit Sampler to target JMeterHelper.executeTest();");
+		System.out.println("Loading config for debugging purposes...");
+		System.out.println("metricsOutputPath:  " + appConfig.getSetting("metricsOutputPath"));
+		System.out.println("jmeterJUnitPageUrl:  " + appConfig.getSetting("jmeterJUnitPageUrl"));
+		System.out.println("driverPath:  " + appConfig.getSetting("driverPath"));
+		System.out.println("standardPageLoadWait:  " + appConfig.getSetting("standardPageLoadWait"));
 	}
 }
